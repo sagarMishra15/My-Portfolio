9ccccd4e8b93ed3d2a635b77144bac877ea1dbbc
SagarMj5 <1998sagar15@gmail.com>
2025-03-31 12:33:31 +0530

    origin

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Box } from '@mui/material'
+import './App.css'
+import { Navbar } from './components/Navbar'
+
+function App() {
+
+  return (
+    <Box>
+      <Navbar/>
+    </Box>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import { Box, Typography } from "@mui/material"
+
+export const Navbar = () => {
+    return (
+      <Box>
+        <Typography variant="body1" sx={{ color: "#03045E" , fontWeight:600, fontSize:24}}>
+          John Doe
+        </Typography>
+      </Box>
+    );
+}
